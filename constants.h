55d14116f37d1ed71b952f74939d75fe71058015
cbliou <charlieliou15@mittymonarch.com>
2020-11-09 01:55:47 -0800

    constants file

---
+++ constants.h
@@ -0,0 +1,7 @@
+#pragma once
+
+uintptr_t godModeAddy = 0x4A7FAE;
+uintptr_t infiniteManaAddy = 0x4A83B9;
+uintptr_t strAddy = 0xE7BD73;
+uintptr_t intAddy = 0xE7BD74;
+uintptr_t dexAddy = 0xE7BD75;
