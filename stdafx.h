23e69debf6ade83d0d0634f165aeec18f27a04f4
cbliou <charlieliou15@mittymonarch.com>
2020-12-03 23:50:47 -0800

    delete unnecessary files

--- stdafx.h
+++
@@ -1,8 +0,0 @@
-// standard imports across the program
-#pragma once
-#include <stdlib.h>
-#include <tchar.h>
-#include <Windows.h>
-#include <vector>
-#include <TlHelp32.h>
-
--- util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-const char* const BoolToString(bool b);
-float round(float number, unsigned int numPlaces);
-
-
-
